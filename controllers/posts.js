e8586b91c287aa8e447829c989feadef524b5be8
ctrlunlimited <david.omogun@gmail.com>
2024-12-08 05:34:05 -0700

    worked on controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -72,6 +72,7 @@ module.exports = {
       let post = await Post.findById({ _id: req.params.id });
       // Delete image from cloudinary
       await cloudinary.uploader.destroy(post.cloudinaryId);
+      
       // Delete post from db
       await Post.remove({ _id: req.params.id });
       console.log("Deleted Post");
